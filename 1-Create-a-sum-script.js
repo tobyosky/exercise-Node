d157ff9e47663d875793a01dbd693029ddbfbd08
Antonio Buzzo <antoniobuzzo002@gmail.com>
2025-03-27 10:41:08 +0100

    add exercise Create a sum script

---
+++ 1-Create-a-sum-script.js
@@ -0,0 +1,7 @@
+function sum(...numbers) {
+  const addends = [...numbers];
+  const sum = addends.reduce((total, addend) => (total += addend), 0);
+  console.log(`The sum of ${addends.join(" + ")} = ${sum}`);
+}
+
+result = sum(5, 6);
